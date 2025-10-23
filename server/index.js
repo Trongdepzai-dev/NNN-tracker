c3613845ae80e0b03f80386933280220e41a292d
B.Trọng <trongha644@gmail.com>
2025-10-24 03:02:18 +0700

    Final Production

--- server/index.js
+++ server/index.js
@@ -5,8 +5,7 @@ import pool from './db.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const PORT = process.env.BACKEND_PORT || 3000;
